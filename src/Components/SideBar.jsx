ddba0bf5ecd674af7d72a125d8245544f02ba6cf
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-09-06 01:23:48 +0530

    Commit 2

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import {Button} from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 import {SideBarOption} from './index';
 import InboxIcon from '@mui/icons-material/Inbox';
@@ -7,8 +6,14 @@ import InboxIcon from '@mui/icons-material/Inbox';
 const SideBar = ()=>{
     return(
         <div className=" flex flex-row w-80 px-3 p-4">
-            <Button className="border-amber-300"><AddIcon/>COMPOSE</Button>
-            <SideBarOption Icon={InboxIcon} tile="Inbox" number={54}/>
+           <div className="flex-col">
+               <div className="bg-gray-700 px-3 py-3 rounded-md text-white cursor-pointer text-center"><AddIcon/> COMPOSE</div>
+               <SideBarOption Icon={InboxIcon} tile="Inbox" number={54} />
+               <SideBarOption Icon={InboxIcon} tile="Start" number={54} />
+               <SideBarOption Icon={InboxIcon} tile="Important" number={54} />
+               <SideBarOption Icon={InboxIcon} tile="Sent" number={54} />
+               <SideBarOption Icon={InboxIcon} tile="Draft" number={54} />
+           </div>
         </div>
     )
 }
--- src/Components/SideBarOption.jsx
+++ src/Components/SideBarOption.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 
-const SideBarOption = ({Icon, title, number})=>{
+const SideBarOption = ({Icon, tile, number})=>{
     return(
-        <div className="">
-
+        <div className="flex h-10 py-3  font-bold mt-1 text-gray-500 hover:bg-red-400 hover:text-yellow-50">
+            <Icon/>
+            <h3 className="flex-1 px-5 font-normal hover:bg-red-400 hover:text-yellow-50 hover:rounded-e-lg">{tile}</h3>
         </div>
     )
 }
