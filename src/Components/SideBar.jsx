173951194ba017fe70fa53caaf986f6baff74fb7
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-09-06 17:58:03 +0530

    Commit 3

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import AddIcon from '@mui/icons-material/Add';
 import {SideBarOption} from './index';
 import InboxIcon from '@mui/icons-material/Inbox';
+import StarIcon from '@mui/icons-material/Star';
 
 const SideBar = ()=>{
     return(
         <div className=" flex flex-row w-80 px-3 p-4">
            <div className="flex-col">
                <div className="bg-gray-700 px-3 py-3 rounded-md text-white cursor-pointer text-center"><AddIcon/> COMPOSE</div>
-               <SideBarOption Icon={InboxIcon} tile="Inbox" number={54} />
-               <SideBarOption Icon={InboxIcon} tile="Start" number={54} />
-               <SideBarOption Icon={InboxIcon} tile="Important" number={54} />
-               <SideBarOption Icon={InboxIcon} tile="Sent" number={54} />
-               <SideBarOption Icon={InboxIcon} tile="Draft" number={54} />
+               <SideBarOption Icon={InboxIcon} tile="Inbox" selected={true}/>
+               <SideBarOption Icon={StarIcon} tile="Started" />
+               <SideBarOption Icon={InboxIcon} tile="Important" />
+               <SideBarOption Icon={InboxIcon} tile="Sent" />
+               <SideBarOption Icon={InboxIcon} tile="Draft"/>
            </div>
         </div>
     )
--- src/Components/SideBarOption.jsx
+++ src/Components/SideBarOption.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 
-const SideBarOption = ({Icon, tile, number})=>{
+const SideBarOption = ({Icon, tile, selected})=>{
     return(
-        <div className="flex h-10 py-3  font-bold mt-1 text-gray-500 hover:bg-red-400 hover:text-yellow-50">
+        <div className={`flex h-10 py-3 p-3  font-bold mt-1 text-gray-500 hover:bg-red-500 hover: cursor-pointer ${selected && "in"}`}>
             <Icon/>
-            <h3 className="flex-1 px-5 font-normal hover:bg-red-400 hover:text-yellow-50 hover:rounded-e-lg">{tile}</h3>
+            <h3 className="flex-1 px-5 font-normal hover:bg-red-500 hover:text-white hover:rounded-e-lg">{tile}</h3>
         </div>
     )
 }
