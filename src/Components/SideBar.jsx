acaba5f28f6977b839038db0d4c9715cd25019fd
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-09-08 23:55:09 +0530

    Commit 1

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -3,6 +3,9 @@ import AddIcon from '@mui/icons-material/Add';
 import {SideBarOption} from './index';
 import InboxIcon from '@mui/icons-material/Inbox';
 import StarIcon from '@mui/icons-material/Star';
+import LabelImportantIcon from "@mui/icons-material/Label";
+import NearMeIcon from "@mui/icons-material/NearMe";
+import NoteIcon from "@mui/icons-material/Note";
 
 const SideBar = ()=>{
     return(
@@ -11,9 +14,9 @@ const SideBar = ()=>{
                <div className="bg-gray-700 px-3 py-3 rounded-md text-white cursor-pointer text-center"><AddIcon/> COMPOSE</div>
                <SideBarOption Icon={InboxIcon} tile="Inbox"/>
                <SideBarOption Icon={StarIcon} tile="Started" />
-               <SideBarOption Icon={InboxIcon} tile="Important" />
-               <SideBarOption Icon={InboxIcon} tile="Sent" />
-               <SideBarOption Icon={InboxIcon} tile="Draft"/>
+               <SideBarOption Icon={LabelImportantIcon} tile="Important" />
+               <SideBarOption Icon={NearMeIcon} tile="Sent" />
+               <SideBarOption Icon={NoteIcon} tile="Draft"/>
            </div>
         </div>
     )
--- src/Components/SideBarOption.jsx
+++ src/Components/SideBarOption.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 const SideBarOption = ({Icon, tile})=>{
     return(
-        <div className={`flex h-10 py-3 p-3  font-bold mt-1 text-gray-500 hover:bg-red-500 hover:text-white cursor-pointer hover:rounded-e-lg`}>
+        <div className={`flex h-10 py-3 font-semibold mt-1 text-gray-500 hover:bg-red-500 hover:text-white cursor-pointer hover:rounded-e-lg`}>
             <Icon className="hover:rounded-e-lg"/>
-            <h3 className="flex-1 px-5 font-normal hover:bg-red-500 hover:text-white hover:rounded-e-lg">{tile}</h3>
+            <h3 className="px-3">{tile}</h3>
         </div>
     )
 }
